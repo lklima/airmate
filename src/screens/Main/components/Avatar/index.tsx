da3695255effeabbe7dd297ffa3f3a5aa4f4e8d9
Lucas Lima <lucas.lldcosta@gmail.com>
2023-04-11 22:32:03 -0300

    finish scale animation

--- src/screens/Main/components/Avatar/index.tsx
+++ src/screens/Main/components/Avatar/index.tsx
@@ -1,61 +1,79 @@
-import React, { useRef } from "react";
-import Animated, {
+import React from "react";
+import { View, useWindowDimensions } from "react-native";
+
+import {
+  Extrapolate,
   SharedValue,
-  runOnJS,
+  interpolate,
+  measure,
+  useAnimatedRef,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
-  withTiming,
 } from "react-native-reanimated";
 
-import {
-  AvatarButtom,
-  AvatarPic,
-  AvatarRow,
-  Container,
-  Header,
-  Title,
-  Gradient,
-  AvatarWrapper,
-} from "./styles";
+import { AvatarButtom, AvatarPic, Wrapper } from "./styles";
 
 import avatar from "../../../../assets/avatar.png";
 
 interface Props {
-  index: number;
-  panStarted: SharedValue<boolean>;
+  initial: boolean;
   viewTranslateX: SharedValue<number>;
   viewTranslateY: SharedValue<number>;
 }
 
-export default function Avatar({
-  index,
-  panStarted,
-  viewTranslateX,
-  viewTranslateY,
-}: Props) {
-  const tranlateX = useSharedValue(0);
-  const tranlateY = useSharedValue(0);
+export default function Avatar({ initial, viewTranslateX, viewTranslateY }: Props) {
+  const { width } = useWindowDimensions();
+  const scale = useSharedValue(1);
 
-  const wrapRef = useRef(null);
-
-  const measure = () => {
-    wrapRef.current.measure((x, y, width, height, pageX, pageY) => {
-      // console.log("x:", pageX, "y:", pageY);
-    });
-  };
+  const wrapRef = useAnimatedRef<View>();
 
   useDerivedValue(() => {
-    if (index === 0 && viewTranslateX.value && viewTranslateX.value) {
-      runOnJS(measure)();
+    if (_WORKLET) {
+      const measured = measure(wrapRef);
+      if (!measured || !viewTranslateX.value || !viewTranslateY.value) return;
+      const { pageX, pageY } = measured;
+
+      let initialTranslate = -90;
+      let finalTranslate = -10;
+      let letValue = pageX;
+
+      if (pageX <= -10) {
+        initialTranslate = -90;
+        finalTranslate = -10;
+        letValue = pageX;
+      } else if (pageX >= 280) {
+        initialTranslate = 365;
+        finalTranslate = 285;
+        letValue = pageX;
+      } else if (pageY <= 80) {
+        initialTranslate = 0;
+        finalTranslate = 80;
+        letValue = pageY;
+      } else if (pageY >= 730) {
+        initialTranslate = 810;
+        finalTranslate = 730;
+        letValue = pageY;
+      }
+
+      scale.value = interpolate(
+        letValue,
+        [initialTranslate, finalTranslate],
+        [0, 1],
+        Extrapolate.CLAMP
+      );
     }
   }, [viewTranslateX.value, viewTranslateY.value]);
 
+  const animateAvatar = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
   return (
-    <AvatarWrapper ref={wrapRef}>
+    <Wrapper ref={wrapRef}>
       <AvatarButtom>
-        <AvatarPic source={avatar} />
+        <AvatarPic source={avatar} style={animateAvatar} />
       </AvatarButtom>
-    </AvatarWrapper>
+    </Wrapper>
   );
 }
--- src/screens/Main/components/Avatar/styles.ts
+++ src/screens/Main/components/Avatar/styles.ts
@@ -1,56 +1,11 @@
-import { LinearGradient } from "expo-linear-gradient";
-import { Animated } from "react-native";
-import styled, { css } from "styled-components/native";
+import Animated from "react-native-reanimated";
+import styled from "styled-components/native";
 
-interface Props {
-  reverse?: boolean;
-}
-
-export const Container = styled.View`
-  flex: 1;
-  background: white;
-`;
-
-export const Header = styled.SafeAreaView`
-  align-items: center;
-  background: white;
-  z-index: 99;
-`;
-
-export const Title = styled.Text`
-  color: black;
-  font-size: 28px;
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
-
-export const Gradient = styled(LinearGradient).attrs({
-  colors: ["white", "rgba(255, 255, 255, 0.119)"],
-  locations: [0.2, 0.8],
-})<Props>`
-  height: 40px;
-  width: 100%;
-  z-index: 99;
-
-  ${({ reverse }) =>
-    reverse &&
-    css`
-      position: absolute;
-      bottom: 0;
-      transform: rotateX(180deg);
-    `}
-`;
-
-export const AvatarWrapper = styled(Animated.View)``;
+export const Wrapper = styled.View``;
 
 export const AvatarButtom = styled.TouchableOpacity.attrs({
   activeOpacity: 0.9,
 })``;
-
-export const AvatarRow = styled.View<Props>`
-  flex-direction: row;
-  margin-left: ${({ reverse }) => (reverse ? 60 : 0)}px;
-`;
 
 export const AvatarPic = styled(Animated.Image)`
   height: 80px;
